5b7f01ef64332987e94ff3b0c941b9e90e5b58bd
hazeli7777 <kaylee@telegraphs.org>
2025-04-10 19:53:03 +0800

    fix build error

--- src/gpu-common.mk
+++ src/gpu-common.mk
@@ -1,7 +1,8 @@
 NVCC:=nvcc
-GPU_PTX_ARCH:=compute_35
-GPU_ARCHS?=sm_37,sm_50,sm_61,sm_70
-GPU_CFLAGS:=--gpu-code=$(GPU_ARCHS),$(GPU_PTX_ARCH) --gpu-architecture=$(GPU_PTX_ARCH)
+# GPU_PTX_ARCH:=compute_35 # Removed old compute capability
+GPU_ARCHS?=sm_50,sm_61,sm_70 # Removed sm_37
+# GPU_CFLAGS:=--gpu-code=$(GPU_ARCHS),$(GPU_PTX_ARCH) --gpu-architecture=$(GPU_PTX_ARCH)
+GPU_CFLAGS:=$(addprefix --gpu-code=,$(GPU_ARCHS)) # Generate flags for each specified architecture
 CFLAGS_release:=--ptxas-options=-v $(GPU_CFLAGS) -O3 -Xcompiler "-Wall -Werror -fPIC -Wno-strict-aliasing"
 CFLAGS_debug:=$(CFLAGS_release) -g
 CFLAGS:=$(CFLAGS_$V)
